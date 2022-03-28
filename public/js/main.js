118003f9c52eaaa9821e740a433825b7b5b6f65d
Rodrigo Rivera <rodrigo.rivera.b@hotmail.com>
2022-03-28 19:40:21 -0300

    cambios

--- public/js/main.js
+++ public/js/main.js
@@ -40,20 +40,20 @@ window.addEventListener('scroll', function () {
 let iconos = document.querySelector(".one");
 iconos.addEventListener("mouseover", function () {
     let item = document.querySelectorAll(".skills-item");
-    item[0].setAttribute("src", "img/icon/php.png");
-    item[1].setAttribute("src", "img/icon/java.png");
-    item[2].setAttribute("src", "img/icon/javascript.png");
-    item[3].setAttribute("src", "img/icon/sass.png");
-    item[4].setAttribute("src", "img/icon/bootstrap.png");
+    item[0].setAttribute("src", "img/icon/react.png");
+    item[1].setAttribute("src", "img/icon/javascript.png");
+    item[2].setAttribute("src", "img/icon/php.png");
+    item[3].setAttribute("src", "img/icon/java.png");
+    item[4].setAttribute("src", "img/icon/sass.png");
     item[5].setAttribute("src", "img/icon/photoshop.png");
 });
 
 iconos.addEventListener("mouseout", function () {
     let item = document.querySelectorAll(".skills-item");
-    item[0].setAttribute("src", "img/icon/phpoff.png");
-    item[1].setAttribute("src", "img/icon/javaoff.png");
-    item[2].setAttribute("src", "img/icon/javascriptoff.png");
-    item[3].setAttribute("src", "img/icon/sassoff.png");
-    item[4].setAttribute("src", "img/icon/bootstrapoff.png");
+    item[0].setAttribute("src", "img/icon/reactoff.png");
+    item[1].setAttribute("src", "img/icon/javascriptoff.png");
+    item[2].setAttribute("src", "img/icon/phpoff.png");
+    item[3].setAttribute("src", "img/icon/javaoff.png");
+    item[4].setAttribute("src", "img/icon/sassoff.png");
     item[5].setAttribute("src", "img/icon/photoshopoff.png");
 });
